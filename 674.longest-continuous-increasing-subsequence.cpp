666c6b9e16e52e356db874ec7f900d2e441efc3f
tzwtzw <tanzhiwen2018@gmail.com>
2019-05-23 22:47:53 +0800

    0523

---
+++ 674.longest-continuous-increasing-subsequence.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=674 lang=cpp
+ *
+ * [674] Longest Continuous Increasing Subsequence
+ */
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int max_l=1,tmp=1;
+        const int n=nums.size();if(n==0)return 0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i+1]>nums[i])
+                max_l=max(++tmp,max_l);
+            else
+                tmp=1;
+        }
+        return max_l;
+    }
+};
+
